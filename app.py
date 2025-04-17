272459924b848aad8d1d84f2da9f790975a73ad9
De1smart <mymumforever2@gmail.com>
2025-04-17 16:10:00 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,44 +1,17 @@
+
 from flask import Flask, request
-import os
 from twilio.twiml.messaging_response import MessagingResponse
-from google.cloud import dialogflow_v2 as dialogflow
-import uuid
+import dialogflow_handler
 
-# Ensure your service account file is recognized
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "dialogflow-service-key.json"
-
-# Initialize Flask app
 app = Flask(__name__)
 
-# Root route to confirm the app is live
-@app.route("/")
-def home():
-    return "Sevo Neera @2026 WhatsApp bot is running."
-
-# Webhook route for Twilio
-@app.route("/webhook", methods=["POST"])
+@app.route('/webhook', methods=['POST'])
 def webhook():
-    incoming_msg = request.values.get("Body", "")
-    phone_number = request.values.get("From", "")
-    
-    session_id = str(uuid.uuid4())
-    session_client = dialogflow.SessionsClient()
-    session = session_client.session_path("sevoneera-2026-bxgr", session_id)
+    incoming_msg = request.values.get('Body', '').strip()
+    resp = MessagingResponse()
+    reply = dialogflow_handler.get_dialogflow_reply(incoming_msg)
+    resp.message(reply)
+    return str(resp)
 
-    text_input = dialogflow.types.TextInput(text=incoming_msg, language_code="en")
-    query_input = dialogflow.types.QueryInput(text=text_input)
-
-    try:
-        response = session_client.detect_intent(request={"session": session, "query_input": query_input})
-        reply = response.query_result.fulfillment_text
-    except Exception as e:
-        reply = "Sorry, something went wrong while contacting Dialogflow."
-        print(f"[ERROR] {e}")
-
-    # Reply via Twilio
-    twilio_response = MessagingResponse()
-    twilio_response.message(reply)
-    return str(twilio_response)
-
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ dialogflow_handler.py
@@ -0,0 +1,21 @@
+
+import os
+import dialogflow_v2 as dialogflow
+from google.api_core.exceptions import InvalidArgument
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "dialogflow-service-key.json"
+DIALOGFLOW_PROJECT_ID = "sevoneera-2026-bxgr"
+DIALOGFLOW_LANGUAGE_CODE = "en"
+SESSION_ID = "current-user-id"
+
+def get_dialogflow_reply(text):
+    session_client = dialogflow.SessionsClient()
+    session = session_client.session_path(DIALOGFLOW_PROJECT_ID, SESSION_ID)
+    text_input = dialogflow.types.TextInput(text=text, language_code=DIALOGFLOW_LANGUAGE_CODE)
+    query_input = dialogflow.types.QueryInput(text=text_input)
+
+    try:
+        response = session_client.detect_intent(request={"session": session, "query_input": query_input})
+        return response.query_result.fulfillment_text
+    except InvalidArgument:
+        return "Something went wrong while contacting Dialogflow."
